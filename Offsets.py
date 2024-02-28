25b77111f5e7010cd94b1a10a73eeabf2b62a62f
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-02-28 14:27:46 +0800

    format

--- Offsets.py
+++ Offsets.py
@@ -1,46 +1,47 @@
-GLOBAL_ADDRESS=0x01D70168 #30 E5 36 8C 2B 02 00 00 05 00 00 00 05 00 00
-#生命
-HEALTH_LIMLI=[GLOBAL_ADDRESS,0x284] #ctypes.c_float
-CURRENT_HEALTH=[GLOBAL_ADDRESS,0x280] #ctypes.c_float
-#护甲
-ARMO=[GLOBAL_ADDRESS,0x150c]
+GLOBAL_ADDRESS = 0x01D70168  # 30 E5 36 8C 2B 02 00 00 05 00 00 00 05 00 00
+# 生命
+HEALTH_LIMLI = [GLOBAL_ADDRESS, 0x284]  # ctypes.c_float
+CURRENT_HEALTH = [GLOBAL_ADDRESS, 0x280]  # ctypes.c_float
+# 护甲
+ARMO = [GLOBAL_ADDRESS, 0x150C]
 
-#恶意值
-BADSPORT=[GLOBAL_ADDRESS+0x1185718,0xDC8, 0xB0, 0xA08] #ctypes.c_float 0x02EF5880
+# 恶意值
+BADSPORT = [GLOBAL_ADDRESS + 0x1185718, 0xDC8, 0xB0, 0xA08]  # ctypes.c_float 0x02EF5880
 
-#视角
-VISUAL_GA=GLOBAL_ADDRESS+0x290B18
-VISUAL=[[VISUAL_GA ,0x0],#载具外 #ctypes.c_ulong 0x02000C80
-    [VISUAL_GA,0x4],#载具内主驾驶 #ctypes.c_ulong
-    [VISUAL_GA,0x8],#载具内副驾驶 #ctypes.c_ulong
-    [VISUAL_GA,0xC],
-    [VISUAL_GA,0x10],
-    [VISUAL_GA,0x14],
-    [VISUAL_GA,0x18],
-    [VISUAL_GA,0x1c]
-    ]#载具内乘客 #ctypes.c_ulong
+# 视角
+VISUAL_GA = GLOBAL_ADDRESS + 0x290B18
+VISUAL = [
+    [VISUAL_GA, 0x0],  # 载具外 #ctypes.c_ulong 0x02000C80
+    [VISUAL_GA, 0x4],  # 载具内主驾驶 #ctypes.c_ulong
+    [VISUAL_GA, 0x8],  # 载具内副驾驶 #ctypes.c_ulong
+    [VISUAL_GA, 0xC],
+    [VISUAL_GA, 0x10],
+    [VISUAL_GA, 0x14],
+    [VISUAL_GA, 0x18],
+    [VISUAL_GA, 0x1C],
+]  # 载具内乘客 #ctypes.c_ulong
 
-#当前视角
-IS_FIRST_PERSON=[GLOBAL_ADDRESS-0x13190] # 0x1D5CFD8 
-#是否在载具内
-IS_IN_CAR=[GLOBAL_ADDRESS,0xE32] #ctypes.c_ubyte
+# 当前视角
+IS_FIRST_PERSON = [GLOBAL_ADDRESS - 0x13190]  # 0x1D5CFD8
+# 是否在载具内
+IS_IN_CAR = [GLOBAL_ADDRESS, 0xE32]  # ctypes.c_ubyte
 
-#武器
-CURRENT_WEAPON=[GLOBAL_ADDRESS+0x2A14C0] #ctypes.c_ulong 0x2011628 
-CLIP_NUM=[GLOBAL_ADDRESS+0x4C]#ctypes.c_ulong 0x1D701B4 
-CURRENT_WEAPON_AMMO=[GLOBAL_ADDRESS+0x48] #ctypes.c_ulong 0x1D701B0
-#设施
-IS_IN_FACLITY=[GLOBAL_ADDRESS+0x29DFC8] #ctypes.c_ulong 0x200E130 
-#是否在线
-IS_ONLINE=[GLOBAL_ADDRESS,0x1520] #ctypes.c_float
-#是否p暂停
-IS_PAUSE=[GLOBAL_ADDRESS+0x28B5F0] #ctypes.c_ulong 0x1FFB758 
-#是否在打字
-IS_TEXTING=[GLOBAL_ADDRESS+0xFC813E]#ctypes.c_ubyte 0x2D382A6 
-#是否home菜单
-IS_HOME_MENU=[GLOBAL_ADDRESS+0xFC8140]#ctypes.c_ubyte 0x2D382A8 
-IS_LOOK_BACK=[GLOBAL_ADDRESS+0x88E0E0]#ctypes.c_ubyte 0x25FE248 
-IS_WEAPONLIST_OPEN=[GLOBAL_ADDRESS+0x29FADC] #0x200FC44 
-IS_SPACE_DOWN=[GLOBAL_ADDRESS+0x893FD0]#ctypes.c_ubyte
+# 武器
+CURRENT_WEAPON = [GLOBAL_ADDRESS + 0x2A14C0]  # ctypes.c_ulong 0x2011628
+CLIP_NUM = [GLOBAL_ADDRESS + 0x4C]  # ctypes.c_ulong 0x1D701B4
+CURRENT_WEAPON_AMMO = [GLOBAL_ADDRESS + 0x48]  # ctypes.c_ulong 0x1D701B0
+# 设施
+IS_IN_FACLITY = [GLOBAL_ADDRESS + 0x29DFC8]  # ctypes.c_ulong 0x200E130
+# 是否在线
+IS_ONLINE = [GLOBAL_ADDRESS, 0x1520]  # ctypes.c_float
+# 是否p暂停
+IS_PAUSE = [GLOBAL_ADDRESS + 0x28B5F0]  # ctypes.c_ulong 0x1FFB758
+# 是否在打字
+IS_TEXTING = [GLOBAL_ADDRESS + 0xFC813E]  # ctypes.c_ubyte 0x2D382A6
+# 是否home菜单
+IS_HOME_MENU = [GLOBAL_ADDRESS + 0xFC8140]  # ctypes.c_ubyte 0x2D382A8
+IS_LOOK_BACK = [GLOBAL_ADDRESS + 0x88E0E0]  # ctypes.c_ubyte 0x25FE248
+IS_WEAPONLIST_OPEN = [GLOBAL_ADDRESS + 0x29FADC]  # 0x200FC44
+IS_SPACE_DOWN = [GLOBAL_ADDRESS + 0x893FD0]  # ctypes.c_ubyte
 
-IS_CHARACTER_SELECT=[GLOBAL_ADDRESS+0x66]#ctypes.c_ubyte
+IS_CHARACTER_SELECT = [GLOBAL_ADDRESS + 0x66]  # ctypes.c_ubyte
--- Utils.py
+++ Utils.py
@@ -1,128 +1,160 @@
 from pygetwindow import getActiveWindow
 from pynput import keyboard, mouse
-from Memory import read_memory,address_by_offsets,get_process_id_by_name
-from  ctypes import c_ulong,c_float,c_ubyte
+from Memory import read_memory, address_by_offsets, get_process_id_by_name
+from ctypes import c_ulong, c_float, c_ubyte
 
 from time import sleep
-from  sys import argv,executable
+from sys import argv, executable
 from os import execl
 
 import Offsets
 import Initial as ini
+
 controller = keyboard.Controller()
-mouse_controller=mouse.Controller()
+mouse_controller = mouse.Controller()
 
 
-def is_in_game():#是否在游戏内
-    if getActiveWindow()!=None:
+def is_in_game():  # 是否在游戏内
+    if getActiveWindow() != None:
         return ini.GAME_NAME in getActiveWindow().title
 
-def is_in_facility():#是否在设施
-    Offsets=[0x200E130]
-    return read_memory(address_by_offsets(Offsets),c_ulong)==1
 
-def is_online(): #0-线上 #1-故事
-    return read_memory(address_by_offsets(Offsets.IS_ONLINE),c_float)==125
+def is_in_facility():  # 是否在设施
+    Offsets = [0x200E130]
+    return read_memory(address_by_offsets(Offsets), c_ulong) == 1
 
-def is_in_car(): #0-载具外 #1-载具内
-    return read_memory(address_by_offsets(Offsets.IS_IN_CAR),c_ubyte)!=0
+
+def is_online():  # 0-线上 #1-故事
+    return read_memory(address_by_offsets(Offsets.IS_ONLINE), c_float) == 125
+
+
+def is_in_car():  # 0-载具外 #1-载具内
+    return read_memory(address_by_offsets(Offsets.IS_IN_CAR), c_ubyte) != 0
+
 
 def is_first_person():
-    return read_memory(address_by_offsets(Offsets.IS_FIRST_PERSON),c_ulong)==4
+    return read_memory(address_by_offsets(Offsets.IS_FIRST_PERSON), c_ulong) == 4
+
 
 def is_need_reload():
-    #print(read_memory(address_by_offsets(Offsets.CLIP_NUM),c_ulong))
-    return read_memory(address_by_offsets(Offsets.CLIP_NUM),c_ulong)==0
+    # print(read_memory(address_by_offsets(Offsets.CLIP_NUM),c_ulong))
+    return read_memory(address_by_offsets(Offsets.CLIP_NUM), c_ulong) == 0
+
 
 def is_pause():
-    return read_memory(address_by_offsets(Offsets.IS_PAUSE),c_ulong)==1
+    return read_memory(address_by_offsets(Offsets.IS_PAUSE), c_ulong) == 1
+
 
 def is_snakcs_none():
-    return ini.SNACKS==0
+    return ini.SNACKS == 0
+
+
 def is_texting():
-    return read_memory(address_by_offsets(Offsets.IS_TEXTING),c_ubyte)==1
+    return read_memory(address_by_offsets(Offsets.IS_TEXTING), c_ubyte) == 1
+
+
 def is_home_open():
-    return read_memory(address_by_offsets(Offsets.IS_HOME_MENU),c_ubyte)==1
+    return read_memory(address_by_offsets(Offsets.IS_HOME_MENU), c_ubyte) == 1
+
+
 def is_look_back():
-    return read_memory(address_by_offsets(Offsets.IS_LOOK_BACK),c_ubyte)==0
+    return read_memory(address_by_offsets(Offsets.IS_LOOK_BACK), c_ubyte) == 0
+
 
 def is_weaponlist_open():
-    return read_memory(address_by_offsets(Offsets.IS_WEAPONLIST_OPEN),c_ubyte)==1
-def is_space_down():
-    return read_memory(address_by_offsets(Offsets.IS_SPACE_DOWN),c_ubyte)==0
-def is_character_select():
-    return read_memory(address_by_offsets(Offsets.IS_CHARACTER_SELECT),c_ubyte)==1
-def crt_health():#当前血量
-    return read_memory(address_by_offsets(Offsets.CURRENT_HEALTH),c_float)
+    return read_memory(address_by_offsets(Offsets.IS_WEAPONLIST_OPEN), c_ubyte) == 1
 
-def health_limit():#血量上限
-    return read_memory(address_by_offsets(Offsets.HEALTH_LIMLI),c_float)
+
+def is_space_down():
+    return read_memory(address_by_offsets(Offsets.IS_SPACE_DOWN), c_ubyte) == 0
+
+
+def is_character_select():
+    return read_memory(address_by_offsets(Offsets.IS_CHARACTER_SELECT), c_ubyte) == 1
+
+
+def crt_health():  # 当前血量
+    return read_memory(address_by_offsets(Offsets.CURRENT_HEALTH), c_float)
+
+
+def health_limit():  # 血量上限
+    return read_memory(address_by_offsets(Offsets.HEALTH_LIMLI), c_float)
+
 
 def crt_weapon():
-    return read_memory(address_by_offsets(Offsets.CURRENT_WEAPON),c_ulong)
+    return read_memory(address_by_offsets(Offsets.CURRENT_WEAPON), c_ulong)
+
 
 def crt_weapon_ammo():
-    return read_memory(address_by_offsets(Offsets.CURRENT_WEAPON_AMMO),c_ulong)
+    return read_memory(address_by_offsets(Offsets.CURRENT_WEAPON_AMMO), c_ulong)
+
 
 def badsport():
-    return read_memory(address_by_offsets(Offsets.BADSPORT),c_float)
+    return read_memory(address_by_offsets(Offsets.BADSPORT), c_float)
+
 
 def ammo_left_times():
     if is_in_car():
-        left_times=2
+        left_times = 2
     else:
-        weapon=crt_weapon()
-        if 0<=weapon<=5 and weapon!=1:
-            left_times=2
-        elif weapon==1:
-            left_times=3
-        elif weapon==6:
-            left_times=7
-        elif weapon>=7:
-            left_times=2
+        weapon = crt_weapon()
+        if 0 <= weapon <= 5 and weapon != 1:
+            left_times = 2
+        elif weapon == 1:
+            left_times = 3
+        elif weapon == 6:
+            left_times = 7
+        elif weapon >= 7:
+            left_times = 2
         else:
-            left_times=6
+            left_times = 6
     return left_times
+
 
 def press_and_release(key):
     controller.press(key)
     controller.release(key)
 
+
 def press(key):
     controller.press(key)
+
 
 def release(key):
     controller.release(key)
 
-def ahk(key,times=1,delay=0.02):
+
+def ahk(key, times=1, delay=0.02):
     for i in range(times):
         press_and_release(key)
     sleep(delay)
 
-def move(x,y):
-    mouse_controller.move(x,y)
+
+def move(x, y):
+    mouse_controller.move(x, y)
 
 
-def restart():#重启脚本
+def restart():  # 重启脚本
     python_executable = executable
     execl(python_executable, python_executable, *argv)
 
-def auto_reload():#自动重启脚本
+
+def auto_reload():  # 自动重启脚本
     sleep(10)
-    while get_process_id_by_name(ini.PROCESS_NAME)==None:
+    while get_process_id_by_name(ini.PROCESS_NAME) == None:
         sleep(3)
-    ini.PID=get_process_id_by_name(ini.PROCESS_NAME)
+    ini.PID = get_process_id_by_name(ini.PROCESS_NAME)
     sleep(10)
     while not is_online():
         sleep(3)
     sleep(15)
     restart()
 
+
 def block():
-    
-    print('blcok')
+
+    print("blcok")
+
 
 def unblock():
-   print("restore")
-
-
+    print("restore")
