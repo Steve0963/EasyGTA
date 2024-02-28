f4b6cdd221924d610115dbdc84c691a7384e5904
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-02-28 14:23:29 +0800

    format

--- Initial.py
+++ Initial.py
@@ -1,22 +1,23 @@
-from  os.path import join,dirname
-#进程名 应用名
-GAME_NAME="Grand Theft Auto V"
-PROCESS_NAME="GTA5.exe"
+from os.path import join, dirname
 
-#应用图标
-ICON_PATH =join(dirname(__file__), 'icon.png')
+# 进程名 应用名
+GAME_NAME = "Grand Theft Auto V"
+PROCESS_NAME = "GTA5.exe"
 
-#是否挂机
-IS_SUSPEND=False
+# 应用图标
+ICON_PATH = join(dirname(__file__), "icon.png")
+
+# 是否挂机
+IS_SUSPEND = False
 X2_PRESSED = False
-LEFT_PRESSED =False
+LEFT_PRESSED = False
 
-CRT_WEAPON=6
+CRT_WEAPON = 6
 
-FIRST_PERSON=4
-THIRD_PERSON=0
+FIRST_PERSON = 4
+THIRD_PERSON = 0
 
-STOP_LISTENING=False
+STOP_LISTENING = False
 
-PID=None
-IS_JUMP=False
+PID = None
+IS_JUMP = False
