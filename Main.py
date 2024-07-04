f28acd4ed6fdbf70ec2f0adaba3dd7e1f9743df8
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-07-04 12:39:44 +0800

    main函数的功能

--- Main.py
+++ Main.py
@@ -1,33 +1,10 @@
-from threading import Thread
-from pystray import Icon, Menu, MenuItem
-from PIL import Image
-from GameFunction import badsport_value
-import Initial as ini
-from Utils import restart, exit_click
-from pynput import keyboard, mouse
-from Input import on_press, on_release, on_click, on_scroll
-
-def create_ui():
-    icon_image = Image.open(ini.ICON_PATH)
-    menu_items = [
-        MenuItem(f"BadSportValue : {badsport_value()}", badsport_value),
-        MenuItem("Reload", restart),
-        MenuItem("Exit", exit_click),
-    ]
-
-    menu = Menu(*menu_items)
-    ini.ICON = Icon(
-        "Easy GTA5", icon_image, menu=menu, title="Created By QuoVadis8 For GTA5"
-    )
-    ini.ICON.run()
-
-def create_listener():
-    key_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-    mouse_listener = mouse.Listener(on_click=on_click, on_scroll=on_scroll)
-    key_listener.start()
-    mouse_listener.start()
-
-if __name__ == "__main__":
-    Thread(target=create_ui).start()
-    Thread(target=create_listener).start()
-    
+from Memory import init as memory_init
+from Scripts import init as script_init
+from Utils import init as util_init
+from Listener import init as listener_init
+from UI import init as ui_init
+memory_init()
+ui_init()
+script_init()
+util_init()
+listener_init()
