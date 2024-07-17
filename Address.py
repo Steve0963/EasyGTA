b356eb2e6e1fd44bd82caffeb03dfd1758ab119f
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-07-17 23:45:34 +0800

    完善所有的特侦码

--- Address.py
+++ Address.py
@@ -2,15 +2,15 @@ class Pointers:
     Global_Ptr = "4C 8D 05 ?? ?? ?? ?? 4D 8B 08 4D 85 C9 74 11"  # 
     World_Ptr = "48 8B 05 ?? ?? ?? ?? 45 ?? ?? ?? ?? 48 8B 48 08 48 85 C9 74 07"  # 0x2E1611
     Visual_Ptr = "48 8B 15 ?? ?? ?? 01 48 85 D2 74 14 83"  # 0x2491a8 #48 8B 15 ?? ?? ?? 01 48 85 D2 74 14 83 F9 07 77 0F 48 63 C1
-    Lookback_Ptr = '58 8A 0D AD 17 FC 01 8A 15 A8 17 FC 01 8A 85 80 30 00 00 F3'  # 0x563e54 58 8A 0D ?? ?? ?? ?? 8A 15 ?? ?? ?? ??
+    Lookback_Ptr = '58 8A 0D ?? ?? ?? ?? 8A 15 ?? ?? ?? ??'  # 0x563e54 58 8A 0D ?? ?? ?? ?? 8A 15 ?? ?? ?? ??
     HomeMenu_Ptr = "48 8B 0D ?? ?? ?? ?? 4C 8B 11 48 8B D0 8B 40 18 44 8B 4A 20"  # 0x146F50C
     PauseMenu_Ptr = "14 38 1D ?? ?? ?? 01 75 0C 38 1D"  # 0x2C5917 14 38 1D ?? ?? ?? 01 75 0C 38 1D ?? ?? ?? 01 75 04 32 C0 EB          
-    TextMenu_Ptr = "0F 95 05 1B E4 A7 02 E8 54 8D E7 FF 84 C0 0F 84 25 01 00 00"  # 0x195FF5 
+    TextMenu_Ptr = "0F 95 05 ?? ?? A7 02 E8 ?? ?? E7 FF 84 C0 0F 84 25 01 00 00"  # 0x195FF5  0F 95 05 17 EF A7 02 E8 F8 88 E7 FF 84 C0 0F 84 25 01 00 00
     Faclity_Ptr = "48 8D 0D ?? ?? ?? ?? B2 01 E9 ?? ?? E3"  # 0x29BA8  48 8D 0D ?? ?? ?? ?? B2 01 E9 ?? ?? E3 00 ?? ?? 48 8B C4 48
-    WeaponType_Ptr = "48 8D 0D ?? ?? ?? 01 4C 8B C3 89 45 20 E8 ?? 3D 17 00 EB 44"  # 0x2C6342
-    Attribute_Ptr = "00 8A 0D 43 EB A8 01 45 84 C0 74 0D 84 C9 75 09 40 84 F6 74"  # 0x10218E7
+    WeaponType_Ptr = "48 8D 0D ?? ?? ?? 01 4C 8B C3 89 45 20 E8"  # 0x2C6342
+    Attribute_Ptr = "00 8A 0D ?? ?? A8 01 45 84 C0 74 0D 84 C9 75 09 40 84 F6 74"  # 0x10218E7 00 8A 0D 6F F4 A8 01 45 84 C0 74 0D 84 C9 75 09 40 84 F6 74
     Selector_Ptr = "00 39 2D ?? ?? ?? 01 41 0F ?? F0 7F 54 40 38"  # 0x1B8BBF 00 39 2D ?? ?? ?? 01 41 0F ?? F0 7F 54 40 38 2D ?? ?? ?? 01
-    FirstPerson_Ptr = "22 8B 0D 92 DD B5 01 85 C9 78 18 83 F8 04 74 0A 83 F9 04 75"
+    FirstPerson_Ptr = "22 8B 0D ?? ?? B5 01 85 C9 78 18 83 F8 04 74 0A 83 F9 04 75"  #22 8B 0D DE C1 B5 01 85 C9 78 18 83 F8 04 74 0A 83 F9 04 75
                       
 
 
